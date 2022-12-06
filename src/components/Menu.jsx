776051dc685fab39cfc37121aa076dc69d7eeaa9
John Doe <johndoe@example.com>
2022-12-06 15:32:22 +0700

    add icon

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,15 +3,18 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
+// sveta добавила в меню пункт "База знаний" + добавила иконку
 export const userMenu = [
   { name: 'Главная', path: '/', icon: '/img/nav/main.png' },
   { name: 'Обучения', path: '/study', icon: '/img/nav/training.png' },
   { name: 'Тестирование', path: '/testing', icon: '/img/nav/testing.png' },
+  { name: 'База знаний', path: '/knowledge', icon: '/img/nav/knowledge.png' },
 ];
 export const adminMenu = [
   { name: 'Новости', path: '/admin/news', icon: '/img/nav/main.png' },
   { name: 'Обучение', path: '/admin/news/?study=true', icon: '/img/nav/main.png' },
   { name: 'Тестирование', path: '/admin/training', icon: '/img/nav/training.png' },
+  { name: 'База знаний', path: '/knowledge', icon: '/img/nav/сertification.png' },
 
   { name: 'Пользователи', path: '/admin/users', icon: '/img/nav/testing.png' },
   { name: 'Отчеты', path: '/admin/reports', icon: '/img/list.svg' },
