ece237d94b79c7f6a9b5d0473e22193d833714f2
John Doe <johndoe@example.com>
2022-12-27 15:54:12 +0700

    add antd fix modal

--- src/components/modals/ModalArticle.jsx
+++ src/components/modals/ModalArticle.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState, useMemo } from 'react';
 
+import { Select } from 'antd';
+
 import Modal from './Modal';
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm, Controller } from 'react-hook-form';
@@ -17,6 +19,9 @@ const ModalArticle = () => {
     const [articleMarks, setArticleMarks] = useState([]);
     const [articleEmployeePosition, setArticleEmployeePosition] = useState([]);
     const [articleDesc, setArticleDesc] = useState('');
+    const [sectionGroupOptions, setSectionGroupOptions] = useState([]);
+    const [sectionOptions, setSectionOptions] = useState([]);
+    const [markOptions, setMarkOptions] = useState([]);
 
     // получить список групп, разделов, должностей, меток
     const {
@@ -35,13 +40,43 @@ const ModalArticle = () => {
         getEmployees:  { data: employees, loading: loadingEmployeePositions, error: errorEmployeePositions, count: employeePositionsCount }   
     } = useSelector((state) => state.employee);
 
+    const updateSectionOptions = (group=articleSectionGroup) => {
+        let sectionOptionsList = [];
+        sections.forEach((item) => {
+            if (group?.id === item.sectionGroup) {
+                sectionOptionsList.push(
+                    <option value={item.id} key={item.id}>
+                        {item.name}
+                    </option>
+                )
+            }
+        })
+
+        setSectionOptions(sectionOptionsList);
+    }
+
     useEffect(() => {
-        console.log(employees);
+        let sectionGroupOptionsList = sectionGroups.map((item) => {
+            return (
+                <option value={item.id} key={item.id}>
+                    {item.name}
+                </option>
+            )
+        })
 
+        let markOptionsList = marks.map((item) => {
+            return {
+                label: item.name,
+                value: item.id
+            }
+        })
 
-
-
-    }, [employees]);
+        console.log(markOptionsList);
+        
+        setSectionGroupOptions(sectionGroupOptionsList);
+        setMarkOptions(markOptionsList);
+        updateSectionOptions();
+    }, [employees, sectionGroups, sections]);
 
     const onArticleNameChange = (e) => {
         setArticleName(e.target.value);
@@ -49,6 +84,19 @@ const ModalArticle = () => {
 
     const onArticleDescChange = (e) => {
         setArticleDesc(e);
+    }
+
+    const onSectionGroupChange = (e) => {
+        let sectionGroup = sectionGroups.find((item) => {
+            return item.id === e.target.value;
+        })
+        setArticleSectionGroup(sectionGroup);
+        updateSectionOptions(sectionGroup);
+    }
+
+    const onSectionChange = (e) => {
+        let section = sections.find((item) => item.id === e.target.value)
+        setArticleSection(section);
     }
 
 
@@ -94,10 +142,11 @@ const ModalArticle = () => {
                 <div>
                     <div className='moadal__article__select-group'>
                         <div className="modal__select">
-                            <select>
+                            <select onChange={onSectionGroupChange}>
                                 <option value={''} selected>
                                     Группа
                                 </option>
+                                {sectionGroupOptions}
                             </select>
                         </div>
                         <div className="modal__create">
@@ -110,10 +159,11 @@ const ModalArticle = () => {
 
                     <div className='moadal__article__select-group'>
                         <div className="modal__select">
-                            <select>
+                            <select onChange={onSectionChange}>
                                 <option value={''} selected>
                                     Раздел
                                 </option>
+                                {sectionOptions}
                             </select>
                         </div>
                         <div className="modal__create">
@@ -126,11 +176,13 @@ const ModalArticle = () => {
 
                     <div className='moadal__article__select-group'>
                         <div className="modal__select">
-                            <select>
-                                <option value={''} selected>
-                                    Метка
-                                </option>
-                            </select>
+                            <Select
+                                mode='multiple'
+                                placeholder="Выберете метки"
+                                options={markOptions}
+                            >
+                            </Select>
+
                         </div>
                         <div className="modal__create">
                             <input type="text" placeholder="Добавить метку" autoComplete="off" />
