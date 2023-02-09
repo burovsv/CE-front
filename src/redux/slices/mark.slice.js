2da41b95ae9551d5845d50c98908a22402a9d535
John Doe <johndoe@example.com>
2023-02-09 10:39:13 +0700

    fix

--- src/redux/slices/mark.slice.js
+++ src/redux/slices/mark.slice.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { initStateGetMarks, reducerGetMarks } from '../actions/knowledgeBase/getMarks.action';
+import { initStateCreateMark, createMark } from '../actions/knowledgeBase/createMark.action';
 
 
 export const initialState = {
@@ -17,25 +18,6 @@ export const markSlice = createSlice({
       resetCreateMark(state) {
         state.createMark = initStateCreateMark.createMark;
       },
-    //   resetCreateTesting(state) {
-    //     state.createTesting = initStateCreateTesting.createTesting;
-    //   },
-    //   resetUpdateTesting(state) {
-    //     state.updateTesting = initStateUpdateTesting.updateTesting;
-    //   },
-    //   resetGetUserTesting(state) {
-    //     state.getUserTesting = initStateGetUserTesting.getUserTesting;
-    //   },
     },
-    // extraReducers: {
-    // ...reducerGetAdminArticles,
-    //   ...reducerCreateTesting,
-    //   ...reducerGetUserTesting,
-    //   ...reducerUpdateTesting,
-    //   ...reducerGetAdminTestingSingle,
-    //   ...reducerGetUserTestingSingle,
-    //   ...reducerDeleteTesting,
-    // },
   });
-//   export const { resetGetAdminTesting, resetGetAdminTestingSingle, resetCreateTesting, resetGetUserTesting, resetUpdateTesting } = testingSlice.actions;
   export const markReducer = markSlice.reducer;
