7a19900b185e5c1e3f9815c51363b9c09698e556
John Doe <johndoe@example.com>
2023-03-21 20:14:19 +0700

    FIX singlepagearticle

--- src/components/pages/KnowledgeBaseSinglePage.jsx
+++ src/components/pages/KnowledgeBaseSinglePage.jsx
@@ -7,12 +7,18 @@ import { useNavigate, useLocation, useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import { ArrowLeftOutlined } from '@ant-design/icons';
 
+import { Collapse } from "antd";
+import PDFViewer from 'pdf-viewer-reactjs'
+import ReactPlayer from 'react-player'
+
 import { getArticlesUser } from '../../redux/actions/knowledgeBase/getArticlesUser.action';
 import { getMarks } from '../../redux/actions/knowledgeBase/getMarks.action';
 import { getSections } from '../../redux/actions/knowledgeBase/getSections.action';
 import { getSectionGroups } from '../../redux/actions/knowledgeBase/getSectionGroups.action';
 
 import Axios from "axios";
+
+const { Panel } = Collapse;
 
 const KnowledgeBaseSinglePage = () => {
     const [name, setName] = useState('');
@@ -22,6 +28,10 @@ const KnowledgeBaseSinglePage = () => {
     const [sectionGroup, setSectionGroup] = useState('');
     const [articleMarks, setArticleMarks] = useState([]);
     const [mainContent, setMainContent] = useState('');
+    const [textFilesList, setTextFilesList] = useState([]);
+    const [videoFilesList, setVideoFilesList] = useState([]);
+
+    const [textFilesElement, setTextFilesElement] = useState('');
 
     const { knowledgeBaseId } = useParams();
 
@@ -55,12 +65,8 @@ const KnowledgeBaseSinglePage = () => {
     useEffect(() => {
         if (!articles) return
 
-
-
         let article = articles.find(el => el.id == knowledgeBaseId);
         let sectionGroupId = article?.section?.sectionGroupId;
-
-        console.log(article);
 
         let sectionGroup = sectionGroups?.find(group => group.id == sectionGroupId );
         let articleMarks = article?.marks ?? [];
@@ -68,12 +74,15 @@ const KnowledgeBaseSinglePage = () => {
         let articleFiles = article?.articleFiles ?? [];
 
         let mainContentUrl = _.find(articleFiles, { 'isMain': true })?.url ?? '';
+        let additionalTextFiles = _.filter(articleFiles, function (el) {
+            return el.isMain == false && el.type != 'video'
+        }) ?? [];
+        let additionalVideoFiles = _.filter(articleFiles, { 'isMain': false, 'type': 'video' }) ?? [];
+
+        setVideoFilesList(additionalVideoFiles)
 
         let url = `${process.env.REACT_APP_SERVER_API}${mainContentUrl}`;
-
         
-        console.log('main', `${process.env.REACT_APP_SERVER_API}${mainContentUrl}`);
-
         // let reader = new FileReader();
         // reader.readAsDataURL(mainContentUrl);
         // reader.onload = function () {
@@ -85,11 +94,27 @@ const KnowledgeBaseSinglePage = () => {
 
         Axios(url).then(res => {
             // setText(res.data)
-            console.log(res.data);
             setMainContent(res.data);
         });
 
+        setTextFilesList([]);
 
+        additionalTextFiles.forEach(data => {
+            let url = `${process.env.REACT_APP_SERVER_API}${data.url}`;
+            // if (data.type == 'pdf') return
+            Axios(url).then(res => {
+                // setText(res.data)
+                let file = {
+                    name: data.name,
+                    content: res.data,
+                    type: data.type,
+                    url: `${process.env.REACT_APP_SERVER_API}${data.url}`
+                }
+
+                setTextFilesList(prev => [...prev, file]);
+            });
+
+        });
 
         setName(article.name);
         setDate(article.date);
@@ -98,14 +123,8 @@ const KnowledgeBaseSinglePage = () => {
         setSectionGroup(sectionGroup.name);
         setArticleMarks(articleMarks);
 
-
-
-
+        // должны обнулить значение article
     }, [articles]);
-
-    useEffect(() => {
-        console.log('mainContent', mainContent);
-    }, [mainContent]);
 
     const element = (
         <div
@@ -140,10 +159,43 @@ const KnowledgeBaseSinglePage = () => {
                 textAlign: 'justify',
         
             }}>
-                {/* <div dangerouslySetInnerHTML={{ __html: addYouTubeIframe(description) }} /> */}
-                <div className="knowledge-base-single-page__main-content" style={{width: '100%'}}> 
+                <div className="knowledge-base-single-page__main-content"> 
 
-                    {mainContent ? (<div dangerouslySetInnerHTML={{ __html: mainContent }} />) : ( "")}
+                    <Collapse>
+                        <Panel header="Основная статья" key="1">
+                            {mainContent ? (<div dangerouslySetInnerHTML={{ __html: mainContent }} />) : ( "")}
+                        </Panel>
+                        <Panel header="Дополнительные текстовые материалы" key="2">
+                            <Collapse ghost>
+                                {textFilesList.map((file, index) => {
+                                    return (
+                                        <Panel header={file.name} key={`2_${index}`}>
+                                            {
+                                                (file.type == 'pdf') 
+                                                    ? (<PDFViewer document={{
+                                                            url: file.url,
+                                                        }} />)
+                                                    : (<div dangerouslySetInnerHTML={{ __html: file.content }} />)
+                                            }
+                                            
+                                        </Panel>
+                                    )
+                                })}
+                            </Collapse>
+                        </Panel>
+                        <Panel header="Дополнительные видео материалы" key="3">
+                        <Collapse ghost>
+                                {videoFilesList.map((file, index) => {
+                                    return (
+                                        <Panel header={file.name} key={`2_${index}`}>
+                                            <p>{file?.description}</p>
+                                            <ReactPlayer url={file.url} />
+                                        </Panel>
+                                    )
+                                })}
+                            </Collapse>
+                        </Panel>
+                    </Collapse>
                 </div>
             </div>
 
