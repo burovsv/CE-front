07682670e3ca533ab27c555dc3bb9cd8a40dfb75
BurovSergey <burovsv@internet.ru>
2023-06-05 13:30:05 +0300

    Add click to accept row

--- src/components/AcceptedList.jsx
+++ src/components/AcceptedList.jsx
@@ -6,11 +6,13 @@ import { getStaffList } from '../redux/actions/employee/getStaffList.action';
 import { getStaffListBySubdivision } from '../redux/actions/employee/getStaffListBySubdivision.action';
 import ModalStaff from './modals/ModalStaff';
 import AcceptedCheckbox from './AcceptedCheckbox';
+import { setActiveCalendarSubdivision } from '../redux/slices/employeeHistory.slice';
 const AcceptedList = () => {
   const dispatch = useDispatch();
   const {
     getStaffList: { loading, data, error },
     getStaffListBySubdivision: { data: staffBySubdivision },
+    getEmployeeUser: { data: dataUser, loading: loadingUser, error: errorUser },
   } = useSelector((state) => state.employee);
   const {
     getEmployeeHistory: { data: employeeHistory },
@@ -21,13 +23,26 @@ const AcceptedList = () => {
       <table>
         <tr class="table-plan-head">
           <th>Подразделение</th>
-          <th>Комментарий</th>
-          <th>Время</th>
-          <th>Согласован</th>
+          <th style={{ textAlign: 'center' }}>Комментарий</th>
+          <th style={{ textAlign: 'center' }}>Время</th>
+          <th style={{ textAlign: 'center' }}>Согласован</th>
         </tr>
         {employeeHistory.map((value) => {
           return (
-            <tr style={{ cursor: 'pointer' }} class={`table-plan-row`}>
+            <tr
+              style={{ cursor: 'pointer' }}
+              class={`table-plan-row`}
+              onClick={() => {
+                if (value.id) {
+                  const findCurrentSubdiv = employeeHistory?.find((historyItem) => historyItem?.id == value.id);
+                  const findAccessSubdiv = dataUser?.subdivisions?.find((accessSubdiv) => accessSubdiv?.id == value.id);
+                  if (findCurrentSubdiv) {
+                    dispatch(setActiveCalendarSubdivision(findCurrentSubdiv));
+                  } else if (findAccessSubdiv) {
+                    dispatch(setActiveCalendarSubdivision({ id: findAccessSubdiv?.id, name: findAccessSubdiv?.name }));
+                  }
+                }
+              }}>
               <td>{value?.name}</td>
               <td></td>
               <td></td>
@@ -37,6 +52,36 @@ const AcceptedList = () => {
             </tr>
           );
         })}
+        {[...dataUser?.subdivisions]
+          ?.sort((a, b) => a.name.localeCompare(b.name))
+          ?.map((itemSubdiv) => {
+            const findRepeat = employeeHistory?.find((emplHist) => emplHist.id == itemSubdiv?.id);
+            if (!findRepeat) {
+              return (
+                <tr
+                  style={{ cursor: 'pointer' }}
+                  class={`table-plan-row`}
+                  onClick={() => {
+                    if (itemSubdiv?.id) {
+                      const findCurrentSubdiv = employeeHistory?.find((historyItem) => historyItem?.id == itemSubdiv?.id);
+                      const findAccessSubdiv = dataUser?.subdivisions?.find((accessSubdiv) => accessSubdiv?.id == itemSubdiv?.id);
+                      if (findCurrentSubdiv) {
+                        dispatch(setActiveCalendarSubdivision(findCurrentSubdiv));
+                      } else if (findAccessSubdiv) {
+                        dispatch(setActiveCalendarSubdivision({ id: findAccessSubdiv?.id, name: findAccessSubdiv?.name }));
+                      }
+                    }
+                  }}>
+                  <td>{itemSubdiv?.name}</td>
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <AcceptedCheckbox />
+                  </td>
+                </tr>
+              );
+            }
+          })}
       </table>
       {staffBySubdivision && <ModalStaff />}
     </>
